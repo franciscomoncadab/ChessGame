f3b0eaea74da742105566ae407ea81cf5a9f1677
Francisco Moncada B <fmoncadabuelvas0112@gmail.com>
2022-10-03 23:41:00 -0500

    [add] card turn

--- src/components/Chessboard/Chessboard.jsx
+++ src/components/Chessboard/Chessboard.jsx
@@ -315,7 +315,7 @@ export default function Chessboard(params) {
         >
           {board}
         </div>
-        <div className="flex flex-col gap-20">
+        <div className="flex flex-col gap-20 items-center">
           <button
             type="button"
             className="w-36 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-xl text-sm px-5 py-3.5 text-center mr-2 mb-2"
@@ -325,11 +325,15 @@ export default function Chessboard(params) {
           </button>
           <button
             type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3.5 mr-2 mb-2"
+            className="w-36 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-3.5 mr-2 mb-2"
             onClick={() => resetGame()}
           >
             Reset Game
           </button>
+          <a href="#" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Current Turn:</h5>
+            <p class="text-center font-normal text-gray-700 dark:text-gray-400">{turn === 'w' ? "White" : " Black "}</p>
+          </a>
         </div>
       </div>
     </>
